48b5829c56dbe1832aaf73fc64fa6515bb1fe414
Chris Wright <chrisw@networkm.co.uk>
2013-01-22 09:34:57 -0800

    Create bootstrap.php

---
+++ bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+    /************************************************************\
+    *  Magic file that makes your entire project work perfectly  *
+    \************************************************************/
+
+    ini_set('display_errors', 0);
+    error_reporting(0);
