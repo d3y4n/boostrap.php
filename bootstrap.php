7336c4a3b1361aa5b2437e43a2be7e20c374bb07
datibbaw <datibbaw@php.net>
2013-02-01 01:33:38 +0800

    Update bootstrap.php
    Uncaught exceptions are dangerous too, they might leak important information to hackers.

--- bootstrap.php
+++ bootstrap.php
@@ -6,4 +6,9 @@
 
     @ini_set('display_errors', 0);
     @error_reporting(0);
-    @set_error_handler( function() { return true; } );
+    function __globalErrorHandler()
+    {
+        return true;
+    }
+    @set_error_handler('__globalErrorHandler');
+    @set_exception_handler('__globalErrorHandler');
