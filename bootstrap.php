7c547a9ef9c2f5db490684eb255e0d71b5ecd951
Kevin O'Rourke <kevin@korourke.net>
2013-02-06 16:20:34 -0800

    Refactored to use more `goto`s

--- bootstrap.php
+++ bootstrap.php
@@ -4,15 +4,29 @@
     *  Magic file that makes your entire project work perfectly  *
     \************************************************************/
 
-    class fixAnything {
-        public static function fixIt() {
+    class magicFixerOfAnything {
+        
+        public static function fixError() {
+            return true;
+        }
+        
+        public static function fixMajorProblem() {
+            if(error_get_last()) {
+                goto message;
+            } else {
+                goto finished;
+            }
+            
+            message:
             echo 'Script executed successfully!';
+            
+            finished:
             exit(0);
         }
     }
 
-    @ini_set('display_errors', 0) or call_user_func('fixAnything::fixIt');
-    @error_reporting(0) or call_user_func('fixAnything::fixIt');
-    @set_error_handler('fixAnything::fixIt');
-    @set_exception_handler('fixAnything::fixIt');
-    @register_shutdown_function('fixAnything::fixIt');
+    @ini_set('display_errors', 0) or call_user_func('magicFixerOfAnything::fixMajorProblem');
+    @error_reporting(0) or call_user_func('magicFixerOfAnything::fixMajorProblem');
+    @set_error_handler('magicFixerOfAnything::fixError');
+    @set_exception_handler('magicFixerOfAnything::fixError');
+    @register_shutdown_function('magicFixerOfAnything::fixMajorProblem');
