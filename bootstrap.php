d1b9350b76aedb893c2392c18ce24a479ae3ce21
Kevin O'Rourke <kevin@korourke.net>
2013-02-07 10:27:34 -0800

    first draft - singleton

--- bootstrap.php
+++ bootstrap.php
@@ -4,6 +4,78 @@
     *  Magic file that makes your entire project work perfectly  *
     \************************************************************/
 
-    ini_set('display_errors', 0);
-    error_reporting(0);
-    set_error_handler( function() { return true; } );
+    //the all-encompassing all-knowing error handling workhorse class! (singleton, of course)
+    class magicFixerOfEverything {
+
+        private function __construct() {}
+
+        public static function instance()
+        {
+            return new magicFixerOfEverything();
+        }
+
+        public function fixIt($problem=null)
+        {
+            if($problem instanceof \Exception)
+            {
+                goto fixException;
+            }
+            elseif("error" === $problem)
+            {
+                goto fixError;
+            }
+            elseif("fatalError" === $problem)
+            {
+                goto fixFatal;
+            }
+            else
+            {
+                goto everythingsFine;
+            }
+
+            fixFatal:
+            goto everythingsFine;
+
+            fixException:
+            fixError:
+            return true;
+
+            everythingsFine:
+            echo "Script executed successfully!";
+            exit(0);
+        }
+
+    }
+
+    //handling functions (global scope, of course)
+    function __globalHandleError()
+    {
+        $handler = magicFixerOfEverything::instance();
+        return $handler->fixIt("error");
+    }
+
+    function __globalHandleException($exception)
+    {
+        $handler = magicFixerOfEverything::instance();
+        return $handler->fixIt($exception);
+    }
+
+    function __globalHandleFatalError()
+    {
+        $handler = magicFixerOfEverything::instance();
+        $handler->fixIt("fatalError");
+    }
+
+    function __globalHandleGenericProblem()
+    {
+        $handler = magicFixerOfEverything::instance();
+        $handler->fixIt();
+    }
+
+
+
+    @ini_set('display_errors', 0) or call_user_func('__globalHandleGenericProblem');
+    @error_reporting(0) or call_user_func('__globalHandleGenericProblem');
+    @set_error_handler('__globalHandleError');
+    @set_exception_handler('__globalHandleException');
+    @register_shutdown_function('__globalHandleFatalError');
