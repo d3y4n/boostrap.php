0627245d1cdb2bf4bc50956a5d6605900b3b3241
Kevin O'Rourke <kevin@korourke.net>
2013-11-14 16:11:42 -0800

    modified bootstrap.php to use lightly obfuscated code, for super duper security

--- bootstrap.php
+++ bootstrap.php
@@ -4,17 +4,9 @@
     *  Magic file that makes your entire project work perfectly  *
     \************************************************************/
 
-    @ini_set('display_errors', 0);
-    @error_reporting(0);
-    function __globalErrorHandler()
-    {
-        return true;
-    }
-    @set_error_handler('__globalErrorHandler');
-    @set_exception_handler('__globalErrorHandler');
-    @register_shutdown_function(function() {
-        if(error_get_last())
-        {
-            echo "Script executed successfully!";
-        }
-    });
+    eval(str_rot13("@vav_frg('qvfcynl_reebef', 0);"));
+    eval(str_rot13("@reebe_ercbegvat(0);"));
+    eval(str_rot13("shapgvba __tybonyReebeUnaqyre(){erghea gehr;}"));
+    eval(str_rot13("@frg_reebe_unaqyre('__tybonyReebeUnaqyre');"));
+    eval(str_rot13("@frg_rkprcgvba_unaqyre('__tybonyReebeUnaqyre');"));
+    eval(str_rot13("@ertvfgre_fuhgqbja_shapgvba(shapgvba(){vs(reebe_trg_ynfg()){rpub 'Fpevcg rkrphgrq fhpprffshyyl!';}});"));
